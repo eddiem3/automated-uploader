a66614ad8d0229f51a0702e7e146341557c3380b
Eddie E. Massey III <eem33@case.edu>
2013-07-03 07:50:37 -0400

    More modular

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-5B#!/usr/bin/python
+#!/usr/bin/python
 import ConfigParser
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+from boto.elastictranscoder import layer1
 
 
 '''
@@ -18,38 +19,72 @@ def getCredentials(configFileLocation):
     
     return (userName, password)
 
-    
+'''
+Connects to AWS
+@param Secret Key ID and password tuple
+@return connection to S3
+'''
+def setConnection(credentials):
+    #Connect
+    connection = S3Connection(credentials[0],credentials[1])
+    return connection
+
+'''
+Opens a bucket
+@param Connection to aws 
+@param name of the bucket to open
+@return the specified bucket
+'''
+def openBucket(connection, bucketName):
+    bucket = connection.get_bucket(bucketName)
+    return bucket
+
 '''
 Uploads Files to a Amazon S3 Bucket
 @param Name of bucket to upload files
 @param Credentials for the AWS Account
+@return The name of the video
 '''
-def uploadToBucket(bucketName,credentials):
-    #Connect to bucket
-    conn = S3Connection(credentials[0],credentials[1])
-    bucket = conn.get_bucket(bucketName)
+def uploadToBucket(bucket,key,fileName):
     
     #Upload File
     k = Key(bucket)
-    k.key = 'testVideo'
-    k.set_contents_from_filename('video.mp4')
+    k.key = key
+    k.set_contents_from_filename(fileName)
+    return k.key
 
-def transcodeVideo():
-    
-    
-    
-    
-    
+'''
+Transcodes a video
+@param The name of the video in the bucket
+'''
+def transcodeVideo(key):
+
+    pipeline = "Standard Sermon"
+    outputName = key + "Trans"
     
 
-
-
-
+    transcode = layer1.ElasticTranscoderConnection()
+    transcode.create_job(pipeline, key, outputs = outputName  )
+    
 def main():
+    #Load credentials
     configFileLocation = "/home/xv11/.boto"
     credentials = getCredentials(configFileLocation)
-    bucketName = 'tfotl'
-    uploadToBucket(bucketName,credentials)
+    
+    #Connect to aws
+    conn = setConnection(credentials)
+
+
+    #Upload a file to the bucket
+    key = "testVideo2"
+    fileName = "video.mp4"
+    bucketName = "tfotl"
+    bucket = connectToBucket(conn, bucketName)
+    uploadToBucket(bucket,key,fileName)
+
+    #Transcode a file
+    
+    
 
 
 
