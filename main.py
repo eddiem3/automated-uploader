06706bb8600a0c8ad576aec40add0aa116a1c6c4
Eddie Massey III <eem33@case.edu>
2013-10-28 22:08:55 -0400

    Refactoring into classes -UT

--- main.py
+++ main.py
@@ -6,79 +6,72 @@ import boto.elastictranscoder
 import json
 from pprint import pprint
 
+class AWSConnection(self):
 
-'''
-Loads the username and password for amazon aws from a config file
-@param Location of the config file
-@return a tuple in the form (username, password)
-'''
-def getCredentials(configFileLocation):
-    config = ConfigParser.ConfigParser()
-    config.read (configFileLocation)
-    
-    userName = config.get('Credentials','aws_access_key_id')
-    password = config.get('Credentials','aws_secret_access_key')
-    
-    return (userName, password)
+    '''
+    Loads the username and password for amazon aws from a config file
+    @param Location of the config file
+    @return a tuple in the form (username, password)
+    '''
+    def __init__(self, configFileLocation):
+        config = ConfigParser.ConfigParser()
+        config.read (configFileLocation)
 
-'''
-Connects to AWS
-@param Secret Key ID and password tuple
-@return connection to S3
-'''
-def setConnection(credentials):
-    #Connect
-    connection = S3Connection(credentials[0],credentials[1])
-    print "Connecting to S3..."
-    return connection
+        print "Loading user credenials..."
 
-'''
-Opens a bucket
-@param Connection to aws 
-@param name of the bucket to open
-@return the specified bucket
-'''
-def openBucket(connection, bucketName):
-    bucket = connection.get_bucket(bucketName)
-    print "Opening" +" " +bucketName +" " +"bucket"
-    return bucket
+        userName = config.get('Credentials','aws_access_key_id')
+        password = config.get('Credentials','aws_secret_access_key')
 
-'''
-Uploads Files to a Amazon S3 Bucket
-@param Name of bucket to upload files
-@param Credentials for the AWS Account
-@return The name of the video
-'''
-def uploadToBucket(bucket,key,fileName):
-    
-    print "Uploading to bucket"
-    #Upload File
-    k = Key(bucket)
-    k.key = key
-    k.set_contents_from_filename(fileName)
-    return k.key
+        print "Connecting to S3 Bucket..."
+        self.connection = S3Connection(credentials[0],credentials[1])
 
-'''
-Transcodes a video
-@param The name of the video in the bucket
-'''
-def transcodeVideo(path):
-    print "Connecting to AWS Elastic Transcoder"
-    
-    transcode =  boto.elastictranscoder.connect_to_region('us-west-2') #connect to elastic transcoder
-    
-    print "Configuring Job"
+    '''
+    Opens a bucket
+    @param Connection to aws 
+    @param name of the bucket to open
+    @return the specified bucket
+    '''
+    def openBucket(self, bucketName):
+        bucket = self.connection.get_bucket(bucketName)
+        print "Opening" +" " +bucketName +" " +"bucket"
+        return bucket
 
-    pipelineId = '1369250428778-u8cpzw' #Id of aws pipeline
+    '''
+    Uploads Files to a Amazon S3 Bucket
+    @param Name of target bucket
+    @param File Key
+    @return File Name
+    '''
+    def uploadToBucket(bucket,key,fileName):
+        
+        print "Uploading to bucket"
     
-    presetList = transcode.list_presets() #dict of all available presets
+        k = Key(bucket)
+        k.key = key
+        k.set_contents_from_filename(fileName)
+        return k.key
+
+     '''
+    Transcodes a video
+    @param The name of the video in the bucket
+    ''' 
+    def transcodeVideo(path):
+        print "Connecting to AWS Elastic Transcoder"
     
-    presets = {} #dictionary for each preset {presetId:description}
-    outputs = [] #output list of dictionarys for a transcoder job
+        transcode =  boto.elastictranscoder.connect_to_region('us-west-2') #connect to elastic transcoder
+        
+        print "Configuring Job"
+        
+        pipelineId = '1369250428778-u8cpzw' #Id of aws pipeline
+        
+        presetList = transcode.list_presets() #dict of all available presets
+        
+        presets = {} #dictionary for each preset {presetId:description}
+        outputs = [] #output list of dictionarys for a transcoder job
     
 
-    numberOfPresets = 3 #first n presets in the list
-
+        numberOfPresets = 3 #first n presets in the list
+    
     #Grab the Id and Description of each n presets in a range
     for x in range(numberOfPresets):
         presetId =  str(presetList['Presets'][x]['Id'])
@@ -129,30 +122,69 @@ def transcodeVideo(path):
         transcode.create_job(pipelineId, transInput, outputs=outputs)
     except Exception, e:
         print e
+
+
+
+class Publisher:
+    
+    def __init__(self, paths, keys, bucket):
+
+        self.paths = paths
+        self.keys = keys
+        self.bucket = bucket
+
+
+    def makeShortUrl(self):
+        return None
+
+    def postFacebook(self):
+        return None
+
+    def postTwitter(self):
+        return None
+
+    def postYoutube(self):
+
+    def postS3(self):
+        AW
+        return None
+
+    def transcodeVideo(self):
+        return None
+
+
+
         
-
-    
 def main():
-    #Load credentials
-    configFileLocation = "/home/cygnss/.boto"
-    credentials = getCredentials(configFileLocation)
     
+    #Load user data
+    parser = argparse.ArgumentParser(description='Media Uploader')
+    parser.add_argument('-i','--input', help = 'List of file paths', nargs='*', required = True)
+    parser.add_argument('k', '--key', help = 'Keys for corresponding file paths, if unspecified then key = input', nargs='*',required = False)
+    parser.add_argument('b', '--bucket', help = 'S3 Bucket to upload to', required = True)
+    args = parser.parse_args()
+
+    inputPaths = args.input
+    inputKeys = args.key
+    bucket = args.bucket
+    
+
+    if len(inputKeys) != 0:
+        if len(inputKeys) != len(inputPaths):
+            print "Error: Each file does not have a corresponding key"
+            
+    if len(inputKeys) == 0:
+        inputKeys = inputPaths
+
+
+    #Load aws credentials
+    configFileLocation = "/home/cygnss/.boto"
+
     #Connect to aws
-    conn = setConnection(credentials)
+    conn = connectToS3(configFileLocation)
+    
 
 
-
-    #Upload a file to the bucket
-    key = "transcodeTest1"
-    '''
-    fileName = "video.mp4"
-    bucketName = "tfotl"
-    bucket = openBucket(conn, bucketName)
-    uploadToBucket(bucket,key,fileName)
-    '''
-
-    #Transcode a file
-    transcodeVideo(key)
     
 
 
